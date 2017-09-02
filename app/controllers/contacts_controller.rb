39d2c197eacf3b2364011d8298c62c1bea66d26f
Becki Srofe <blf_23@yahoo.com>
2017-09-02 15:39:14 -0600

    Finish contacts

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,18 @@
+class ContactsController < ApplicationController
+  def new
+    @company = Company.find(params[:company_id])
+    @contact = Contact.new
+  end
+
+  def create
+    @company = Company.find(params[:company_id])
+    @contact = @company.contacts.create(contact_params)
+    redirect_to company_path(@company)
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :position, :email, :company_id)
+  end
+end
---
+++ spec/features/contacts/user_can_create_a_new_contact_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "user visits a company show page" do
+  scenario "user creates a new contact" do
+    company = Company.create(name: "ESPN")
+    category = Category.create(title: "Web Development")
+    job = company.jobs.create(title: "developer", description: "super fun", level_of_interest: 90,
+                              city: "New York", category: category)
+
+  visit company_path(company)
+  click_on "Add Contact"
+
+  expect(current_path).to eq(new_company_contact_path(company))
+  fill_in "Name", with: "Person"
+  fill_in "Position", with: "Boss"
+  fill_in "Email", with: "boss@company.com"
+  click_on "Create Contact"
+
+  expect(current_path).to eq(company_jobs_path(company))
+  save_and_open_page
+  expect(page).to have_content("Person")
+  expect(page).to have_content("Boss")
+  expect(page).to have_content("boss@company.com")
+  end
+end
